94f3bc89e8994250693b5715a734ccfa311984f2
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2022-07-07 02:39:21 +0300

    script 4 sds

--- 1011MalzemeScript4.js
+++ 1011MalzemeScript4.js
@@ -1,4 +1,4 @@
-var select = document.querySelector('variant-0');
+var select = document.querySelector(".form-control[data-group-id='12']");
 
 select.addEventListener('change', () => 
     location.reload(),
@@ -7,33 +7,25 @@ select.addEventListener('change', () =>
 
 
 
+$(function(){
+    $(".form-control[data-group-id='12']").trigger('change'); 
+    $(".form-control[data-group-id='12']").change(function(){
+        location.reload(),
+        console.log('refreshed')          
+    });
+});
+
+
+
+
+
 
 // $(document).ready(() => {
 
-//     if($(".product-customization-group[data-group-id='2']").length > 0){
+//     if($(".form-control[data-group-id='12']").length > 0){
 
-//         $(".product-right .product-cart-buttons .product-qty[data-selector='qty-wrapper'] [data-selector='decrease-qty']").addClass("disabled");
-//         $(".product-right .product-cart-buttons .product-qty[data-selector='qty-wrapper'] [data-selector='qty']").addClass("disabled");
-//         $(".product-right .product-cart-buttons .product-qty[data-selector='qty-wrapper'] [data-selector='increase-qty']").addClass("disabled");
-//         $(".variant-group-title[data-group-id='12']").find(".product-customization-field[data-field-id='15']").addClass("d-none");
-//         $(".product-right .quick-order-button[data-selector='add-to-cart']").removeAttr("data-selector").attr("style", "cursor: not-allowed !important;");
+//         $(".form-control[data-group-id='12']").trigger('change');
 
-
-//         $(".product-customization-group[data-group-id='2']").find(".product-customization-field[data-field-id='5'] input").attr("id", "quantity");
-//         $(".product-customization-group[data-group-id='2']").find(".product-customization-field[data-field-id='6'] input").attr("id", "length");
-//         $(".product-customization-group[data-group-id='2']").find(".product-customization-field[data-field-id='15'] input").addClass("disabled").attr("id", "customizationID");
-
-//         setInputFilter($(".product-customization-group[data-group-id='2'] #quantity"), function(value) {
-//             return /^\d*\.?\d*$/.test(value);
-//         });
-
-//         setInputFilter($(".product-customization-group[data-group-id='2'] #length"), function(value) {
-//             return /^\d*\.?\d*$/.test(value);
-//         });
-
-//         $(".product-right .product-cart-buttons").before(`<div class="alert alert-warning text-center" data-selector="ext-info" role="alert">
-//             Ürünün uzunluk ve adet bilgilerini yukarıdaki kutucuğa giriniz.
-//         </div>`);
 
 //     }
 
